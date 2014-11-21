d27baa46c0f99c9f3a396ddadb46b103babbf7dc
Tanya Cashorali <tcashorali@comlinkdata.com>
2014-11-21 18:23:47 -0500

    converted from dict to lists because order matters

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ def index():
 
     ids = set(ids)
     ids = list(ids)
-   
+
     if(len(ids) == 0):
         return render_template('index.html', title='No Data Yet', error='No Live Games')
     else:
@@ -48,7 +48,7 @@ def index():
             teams = box_score.find('tr', {'class':'team-color-strip'})
             team1 = teams.findAll('th')[1].contents[1]
             team2 = teams.findAll('th')[2].contents[1]
-            teams = {"Team1": team1, "Team2": team2}
+            teams = [team1, team2]
             stats = box_score.findAll('td')
             stat_list = []
 
@@ -61,76 +61,78 @@ def index():
             first_down_i = stat_list.index('1st Downs')
             first_downs_a = stat_list[first_down_i + 1]
             first_downs_b = stat_list[first_down_i + 2]
-            first_downs = {'1st DownsA': first_downs_a, '1st DownsB': first_downs_b}
+            first_downs = [first_downs_a, first_downs_b]
             
             third_down_i = stat_list.index('3rd down efficiency')
             third_down_a = stat_list[third_down_i + 1]
             third_down_b = stat_list[third_down_i + 2]
-            third_downs = {'3rd DownsA': third_down_a, '3rd DownsB': third_down_b}
+            third_downs = [third_down_a, third_down_b]
 
             fourth_down_i = stat_list.index('4th down efficiency')
             fourth_down_a = stat_list[fourth_down_i + 1]
             fourth_down_b = stat_list[fourth_down_i + 2]
-            fourth_downs = {'4th DownsA': fourth_down_a, '4th DownsB': fourth_down_b}
+            fourth_downs = [fourth_down_a, fourth_down_b]
 
             total_yards_i = stat_list.index('Total Yards')
             total_yards_a = stat_list[total_yards_i+1]
             total_yards_b = stat_list[total_yards_i+2]
-            total_yards = {'Total YardsA': total_yards_a, 'Total YardsB': total_yards_b}	    
-  
+            total_yards = [total_yards_a, total_yards_b]	    
+
             passing_i = stat_list.index('Passing')
             passing_a = stat_list[passing_i+1]
             passing_b = stat_list[passing_i+2]
-            passing = {'PassingA': passing_a, 'PassingB': passing_b}
+            passing = [passing_a, passing_b]
 
             comp_att_i = stat_list.index('Comp-Att')
             comp_att_a = stat_list[comp_att_i+1]
             comp_att_b = stat_list[comp_att_i+2]
-            comp_att = {'CompletionAttA': comp_att_a, 'CompletionAttB': comp_att_b}
+            comp_att = [comp_att_a, comp_att_b]
 
             rushing_i = stat_list.index('Rushing')
             rushing_a = stat_list[rushing_i+1]
             rushing_b = stat_list[rushing_i+2]
-            rushing = {'RushingA': rushing_a, 'RushingB': rushing_b}
+            rushing = [rushing_a, rushing_b]
 
             rushinga_i = stat_list.index('Rushing Attempts')
             rushinga_a = stat_list[rushinga_i+1]
             rushinga_b = stat_list[rushinga_i+2]
-            rushinga = {'RushingAA': rushinga_a, 'RushingAB': rushinga_b}
+            rushinga = [rushinga_a, rushinga_b]
 
             yards_per_rush_i = stat_list.index('Yards per rush')
             yards_per_rush_a = stat_list[yards_per_rush_i+1]
             yards_per_rush_b = stat_list[yards_per_rush_i+2]
-            yards_per_rush = {'YardsPerRushA': yards_per_rush_a, 'YardsPerRushB': yards_per_rush_b}
+            yards_per_rush = [yards_per_rush_a, yards_per_rush_b]
 
             penalties_i = stat_list.index('Penalties')
             penalties_a = stat_list[penalties_i+1]
             penalties_b = stat_list[penalties_i+2]
-            penalties = {'PenaltiesA': penalties_a, 'PenaltiesB': penalties_b}
+            penalties = [penalties_a, penalties_b]
 
             turnovers_i = stat_list.index('Turnovers')
             turnovers_a = stat_list[turnovers_i+1]
             turnovers_b = stat_list[turnovers_i+2]
-            turnovers = {'TurnoversA': turnovers_a, 'TurnoversB': turnovers_b}
+            turnovers = [turnovers_a, turnovers_b]
 
             fumbles_lost_i = stat_list.index('Fumbles lost')
             fumbles_lost_a = stat_list[fumbles_lost_i+1]
             fumbles_lost_b = stat_list[fumbles_lost_i+2]
-            fumbles_lost = {'FumblesLostA': fumbles_lost_a, 'FumblesLostB': fumbles_lost_b}
+            fumbles_lost = [fumbles_lost_a, fumbles_lost_b]
 
             ints_thrown_i = stat_list.index('Interceptions thrown')
             ints_thrown_a = stat_list[ints_thrown_i+1]
             ints_thrown_b = stat_list[ints_thrown_i+2]
-            ints_thrown = {'IntsThrownA': ints_thrown_a, 'IntsThrownB': ints_thrown_b}
+            ints_thrown = [ints_thrown_a, ints_thrown_b]
 
             possession_i = stat_list.index('Possession')
             possession_a = stat_list[possession_i+1]
             possession_b = stat_list[possession_i+2]
-            possession = {'PossessionA': possession_a, 'PossessionB': possession_b}
-	    
-            result = pd.DataFrame.from_dict({'1st Downs': first_downs.values(), '3rd Downs': third_downs.values(), '4th Downs': fourth_downs.values(), 'Total Yards': total_yards.values(), 'Passing Yards': passing.values(), 'Completion Attempts': comp_att.values(), 'Rushing': rushing.values(), 'Rushing Attempts': rushinga.values(), 'Yards Per Rush': yards_per_rush.values(), 'Penalties': penalties.values(), 'Turnovers': turnovers.values(), 'Fumbles Lost': fumbles_lost.values(), 'Ints Thrown': ints_thrown.values(), 'Possession': possession.values() }, orient='index')            
-            result.columns = teams.values()
-	
+            possession = [possession_a, possession_b]
+        
+            data = [first_downs, third_downs, fourth_downs, total_yards, passing, comp_att, rushing, rushinga, yards_per_rush, penalties, turnovers, fumbles_lost, ints_thrown, possession]
+            result = pd.DataFrame(data)
+            result.index = ['First Downs', 'Third Downs', 'Fourth Downs', 'Total Yards', 'Passing', 'Completion Attempts', 'Rushing', 'Rushing Attempts', 'Yards Per Rush', 'Penalties', 'Turnovers', 'Fumbles Lost', 'Ints Thrown', 'Possession']    
+            result.columns = teams
+
         results.append(result)
                 # print 'Category,' + team1 + ',' + team2
                 # print '1st Downs, ' + first_downs_a + ',' + first_downs_b
