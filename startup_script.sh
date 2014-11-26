e0c420130390555971b2f460070357662309cb14
EC2 Default User <ec2-user@ip-10-169-34-98.ec2.internal>
2014-11-26 19:19:49 +0000

    script to install all packages on EC2 launch

---
+++ startup_script.sh
@@ -0,0 +1,21 @@
+sudo yum -y update
+sudo yum -y install git
+sudo yum -y install gcc
+sudo yum -y install gcc-c++
+sudo yum -y install libxslt-devel
+sudo yum -y install libxml2-devel
+sudo yum -y install gpgme-devel
+sudo yum -y install Cython
+sudo yum -y install python-devel
+
+sudo easy_install BeautifulSoup4
+sudo easy_install pip
+sudo pip install pandas
+sudo easy_install flask
+sudo easy_install fast
+sudo easy_install lxml
+
+git clone https://github.com/tcash21/sports.git
+cd sports
+sqlite3 sports.db < sports.sql 
+
