1028e38e0a6e19106df19ee32030a325f8f6bdd8
Tanya Cashorali <tcashorali@comlinkdata.com>
2014-12-17 15:03:53 -0500

    updates for insert statements

--- scrapeNCAA.py
+++ scrapeNCAA.py
@@ -98,9 +98,10 @@ def index():
             except sqlite3.IntegrityError:
                 print('Error inserting data')
             try:
-                db.execute('''INSERT INTO NCAAstats(game_id, team, first_downs, third_downs, fourth_downs, total_yards, passing, comp_att,
-                                                    yards_per_pass, rushing, rushing_attempts, yards_per_rush, penalties, turnovers, fumbles_lost, ints_thrown,
-                                                    possession ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (halftime_ids[i], team1, cleaned1[2], cleaned1[3], cleaned1[4], int(cleaned1[5]), int(cleaned1[6]), int(cleaned1[7]), int(cleaned1[8]), int(cleaned1[9]), int(cleaned1[10]), int(cleaned1[11]), int(cleaned1[12]), int(cleaned1[13]), cleaned1[14], cleaned[15], cleaned[16]))
+                db.execute('''INSERT INTO NCAAstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pts ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)''', (halftime_ids[i], team1, cleaned1[2], cleaned1[3], cleaned1[4], int(cleaned1[5]), int(cleaned1[6]), int(cleaned1[7]), int(cleaned1[8]), int(cleaned1[9]), int(cleaned1[10]), int(cleaned1[11]), int(cleaned1[12]), int(cleaned1[13]))
+                db.commit()
+            try:
+                db.execute('''INSERT INTO NCAAstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pts ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)''', (halftime_ids[i], team2, cleaned2[2], cleaned2[3], cleaned2[4], int(cleaned2[5]), int(cleaned2[6]), int(cleaned2[7]), int(cleaned2[8]), int(cleaned2[9]), int(cleaned2[10]), int(cleaned2[11]), int(cleaned2[12]), int(cleaned2[13]))
                 db.commit()
             except sqlite3.IntegrityError:
                 print('Error inserting data')
