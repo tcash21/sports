112c5e884750b639015327ea82282791e5c0a42e
Tanya Cashorali <tcashorali@comlinkdata.com>
2014-12-29 15:48:47 -0500

    easier way of grabbing team stats

--- scrapeNCAAteams.py
+++ scrapeNCAAteams.py
@@ -17,45 +17,48 @@ def index():
     print "entered index"
     times = []
     halftime_ids = []
-    url = urllib2.urlopen('http://espn.go.com/mens-college-basketball/teams')
+    url = urllib2.urlopen('http://scores.espn.go.com/ncb/scoreboard')
     soup = bs(url.read(), ['fast', 'lxml'])
-    links = soup.findAll('a', href=re.compile('stats.*'))
+    links = soup.findAll('a', href=re.compile('preview.*'))
     urls = [link.get('href') for link in links]
     link_strings = [l['href'] for l in links]
     ## freeze updates at the half
     for i in range(0, len(link_strings)):
+        print str(i) + ' out of ' + str(len(link_strings)) + ' teams.'
         espn = 'http://espn.go.com' + link_strings[i]
-        id = re.search('=(\d+)', link_strings[i]).group(1)
+        #id = re.search('=(\d+)', link_strings[i]).group(1)
         url = urllib2.urlopen(espn)
         soup = bs(url.read(), ['fast', 'lxml'])
-        totals = soup.find_all('tr', {'class':'total'})[1]
+        teams = soup.findAll('td', {'class':'team'})
+        team1 = teams[0].strong.text
+        team2 = teams[1].strong.text
+        team1_id = re.search('id/(\d+)/', teams[0].a['href']).group(1)
+        team2_id = re.search('id/(\d+)/', teams[1].a['href']).group(1)
+        team1_url = urllib2.urlopen('http://espn.go.com/mens-college-basketball/team/stats/_/id/' + team1_id)
+        soup1 = bs(team1_url.read(), ['fast', 'lxml'])
+        totals = soup1.find_all('tr', {'class':'total'})[1]
         data = totals.findAll('td')
         values = [d.text for d in data]
-        cols = soup.findAll('tr', {'class':'colhead'})[1]
+        cols = soup1.findAll('tr', {'class':'colhead'})[1]
         colnames = cols.findAll('a')
         colnames = [c.text for c in colnames]
-        clubhouse = urllib2.urlopen('http://espn.go.com/mens-college-basketball/team/_/id/' + str(id))
-        clubsoup = bs(clubhouse.read(), ['fast', 'lxml'])
-        recap = clubsoup.findAll('a', href=re.compile('boxscore'))[0]['href']
-        a_url = urllib2.urlopen('http://espn.go.com' + recap)
-        recapsoup = bs(a_url.read(), ['fast', 'lxml'])
-        team = recapsoup.findAll('td', {'class':'team'})
-        try:
-            id1 = int(re.search('id/(\d+)/', str(team[1])).group(1))
-        except:
-            print 'No ID'
-        try:
-            id2 = int(re.search('id/(\d+)/', str(team[2])).group(1))
-        except:
-            print 'No ID'
-        abbr = ''
-        if id == id1:
-            abbr = team[1].text
-        else:
-            abbr = team[2].text
         try:
             with db:
-                db.execute('''INSERT INTO NCAAseasonstats(team, the_date, min, fgm, fga, ftm, fta, tpm, tpa, pts, offr, defr, reb, ast, turnovers, stl, blk) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (abbr, time.strftime("%m/%d/%Y"),values[1],int(values[2]),int(values[3]),int(values[4]),int(values[5]),int(values[6]),int(values[7]),int(values[8]),int(values[9]),int(values[10]),int(values[11]),int(values[12]),int(values[13]),int(values[14]),int(values[15])))
+                db.execute('''INSERT INTO NCAAseasonstats(team, the_date, min, fgm, fga, ftm, fta, tpm, tpa, pts, offr, defr, reb, ast, turnovers, stl, blk) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (team1, time.strftime("%m/%d/%Y"),values[1],int(values[2]),int(values[3]),int(values[4]),int(values[5]),int(values[6]),int(values[7]),int(values[8]),int(values[9]),int(values[10]),int(values[11]),int(values[12]),int(values[13]),int(values[14]),int(values[15])))
+                db.commit()
+        except sqlite3.IntegrityError:
+            print 'Error inserting data'
+        team2_url = urllib2.urlopen('http://espn.go.com/mens-college-basketball/team/stats/_/id/' + team2_id)
+        soup2 = bs(team2_url.read(), ['fast', 'lxml'])
+        totals = soup2.find_all('tr', {'class':'total'})[1]
+        data = totals.findAll('td')
+        values = [d.text for d in data]
+        cols = soup2.findAll('tr', {'class':'colhead'})[1]
+        colnames = cols.findAll('a')
+        colnames = [c.text for c in colnames]
+        try:
+            with db:
+                db.execute('''INSERT INTO NCAAseasonstats(team, the_date, min, fgm, fga, ftm, fta, tpm, tpa, pts, offr, defr, reb, ast, turnovers, stl, blk) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (team2, time.strftime("%m/%d/%Y"),values[1],int(values[2]),int(values[3]),int(values[4]),int(values[5]),int(values[6]),int(values[7]),int(values[8]),int(values[9]),int(values[10]),int(values[11]),int(values[12]),int(values[13]),int(values[14]),int(values[15])))
                 db.commit()
         except sqlite3.IntegrityError:
             print 'Error inserting data'
