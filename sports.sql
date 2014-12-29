5e0b7e06efebb9dcb30b671ff2cb299efd97776f
Tanya Cashorali <tcashorali@comlinkdata.com>
2014-12-29 14:50:57 -0500

    updated sql

--- sports.sql
+++ sports.sql
@@ -27,3 +27,47 @@ create table sports.stats(
 		ints_thrown INT NOT NULL,
 		possession TEXT NOT NULL
 );
+
+CREATE TABLE NCAAseasonstats(
+		team CHAR(5) NOT NULL,
+		the_date TEXT INT NOT NULL,
+		min TEXT NOT NULL,
+		fgm INT NOT NULL,
+		fga INT NOT NULL,
+		ftm INT NOT NULL,
+		fta INT NOT NULL,
+		tpm INT NOT NULL,
+		tpa INT NOT NULL,
+		pts INT NOT NULL,
+		offr INT NOT NULL,
+		defr INT NOT NULL,
+		reb INT NOT NULL,
+		ast INT NOT NULL,
+		turnovers INT NOT NULL,
+		stl INT NOT NULL,
+                blk INT NOT NULL
+);
+
+CREATE TABLE NCAAstats(
+    game_id INT NOT NULL,
+    team CHAR(5) NOT NULL,
+    fgma TEXT NOT NULL,
+    tpma TEXT NOT NULL,
+    ftma TEXT NOT NULL,
+    oreb INT NOT NULL,
+    dreb INT NOT NULL,
+    reb INT NOT NULL,
+    ast NUMERIC NOT NULL,
+    stl INT NOT NULL,
+    blk INT NOT NULL,
+    turnovers INT NOT NULL,
+    pf INT NOT NULL,
+    pts INT NOT NULL
+);
+
+CREATE TABLE NCAAgames(
+        game_id INT PRIMARY KEY NOT NULL,
+        team1 CHAR(5) NOT NULL,
+        team2 CHAR(5) NOT NULL,
+        game_date TEXT NOT NULL
+);
