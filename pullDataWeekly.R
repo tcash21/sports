fd11b2d6d9d229b7d7c39b62f90ca1abaa92e35e
EC2 Default User <ec2-user@ip-10-169-34-98.ec2.internal>
2015-02-05 18:10:18 -0500

    stuff

--- pullDataWeekly.R
+++ pullDataWeekly.R
@@ -40,8 +40,8 @@ la$key <- paste(la$espn_abbr, la$game_date)
 lh$key <- paste(lh$espn_abbr, lh$game_date)
 m3a<-merge(m2, la, by="key")
 m3h<-merge(m2, lh, by="key")
-colnames(m3a)[44] <- "CoversTotalLineUpdateTime"
-colnames(m3h)[44] <- "CoversTotalLineUpdateTime"
+colnames(m3a)[46] <- "CoversTotalLineUpdateTime"
+colnames(m3h)[46] <- "CoversTotalLineUpdateTime"
 
 ## Halftime Lines
 la2<-merge(lookup, halflines, by="away_team")
@@ -50,29 +50,34 @@ la2$key <- paste(la2$espn_abbr, la2$game_date)
 lh2$key <- paste(lh2$espn_abbr, lh2$game_date)
 m3a2<-merge(m2, la2, by="key")
 m3h2<-merge(m2, lh2, by="key")
-colnames(m3a2)[44] <- "CoversHalfLineUpdateTime"
-colnames(m3h2)[44] <- "CoversHalfLineUpdateTime"
+colnames(m3a2)[46] <- "CoversHalfLineUpdateTime"
+colnames(m3h2)[46] <- "CoversHalfLineUpdateTime"
 
 l<-merge(m3a, m3a2, by=c("game_date.y", "away_team"))
-m3a<-cbind(m3a, l[,84:86])
+l<-l[match(m3a$key, l$key.y),]
+m3a<-cbind(m3a, l[,88:90])
 l2<-merge(m3h, m3h2, by=c("game_date.y", "home_team"))
-m3h<-cbind(m3h, l2[,84:86])
+l2<-l2[match(m3h$key, l2$key.y),]
+m3h<-cbind(m3h, l2[,88:90])
 
-colnames(m3h)[39:40] <- c("home_team.x", "home_team.y")
-colnames(m3a)[36] <- "home_team"
+colnames(m3h)[41:42] <- c("home_team.x", "home_team.y")
+colnames(m3a)[38] <- "home_team"
 all <- rbind(m3a, m3h)
 all <- all[,-1]
 all$key <- paste(all$game_id, all$team.y)
 all<-all[match(unique(all$key), all$key),]
 
-colnames(all) <- c("GAME_ID","TEAM","FGM-A","3PM-A","FTM-A","OREB","DREB","REB","AST","STL","BLK","TO","PF","PTS","TEAM1","TEAM2","GAME_DATE","REMOVE1","REMOVE2","REMOVE3",
-"SEASON_FGM","SEASON_FGA","SEASON_FTM","SEASON_FTA","SEASON_3PM","SEASON_3PA","SEASON_PTS","SEASON_OFFR","SEASON_DEFR","SEASON_REB","SEASON_AST","SEASON_TO","SEASON_STL",
-"SEASON_BLK","REMOVE4","REMOVE5","REMOVE6","REMOVE7","REMOVE8","REMOVE9","LINE", "SPREAD", "COVERS_UPDATE","LINE_HALF", "SPREAD_HALF", "COVERS_HALF_UPDATE", "REMOVE11")
+colnames(all) <- c("GAME_ID","TEAM","FGM-A","3PM-A","FTM-A","OREB","DREB","REB","AST","STL","BLK","TO","PF","PTS","BOXSCORE_TIMESTAMP", "TEAM1","TEAM2","GAME_DATE",
+"GAME_TIME","REMOVE2","REMOVE3","MIN", "SEASON_FGM","SEASON_FGA","SEASON_FTM","SEASON_FTA","SEASON_3PM","SEASON_3PA","SEASON_PTS","SEASON_OFFR","SEASON_DEFR","SEASON_REB",
+"SEASON_AST","SEASON_TO","SEASON_STL", "SEASON_BLK","REMOVE4","REMOVE5","REMOVE6","REMOVE7","REMOVE8","REMOVE9","LINE", "SPREAD", "COVERS_UPDATE","LINE_HALF", 
+"SPREAD_HALF", "COVERS_HALF_UPDATE", "REMOVE11")
 all <- all[,-grep("REMOVE", colnames(all))]
 
 all[,3]<-paste0("=\"", all[,3], "\"")
 all[,4]<-paste0("=\"", all[,4], "\"")
 all[,5]<-paste0("=\"", all[,5], "\"")
+
+all<-all[order(all$GAME_DATE, decreasing=TRUE),]
 
 write.csv(all, file="testfile.csv", row.names=FALSE)
 
@@ -84,7 +89,7 @@ subject="Subject of the email",
 body = "Body of the email",
 smtp = list(host.name = "aspmx.l.google.com", port = 25),
 authenticate = FALSE,
-attach.files = c("/home/ec2-user/testfile.csv"),
+attach.files = c("/home/ec2-user/sports/testfile.csv"),
 send = FALSE)
 email$send() # execute to send email
 
